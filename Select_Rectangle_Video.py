579d24457f991564feb9495f99044d45d9133104
Максим Валериевич Шишков <ShiMaxVal@yandex.ru>
2023-01-07 02:38:55 +0700

    SelectRectangleVideo добавил вывод теста

--- Select_Rectangle_Video.py
+++ Select_Rectangle_Video.py
@@ -1,13 +1,15 @@
 import numpy as np
 import cv2
 green_rect = 0
-cross_way =[{}]*4
+cross_way =[{}]*5
 cross_way[0] = {"Forward"}
 cross_way[1] = {"Left"}
 cross_way[2] = {"Right"}
 cross_way[3] = {"Backward"}
+cross_way[4] = {"Error"}
 capImg = cv2.VideoCapture(0)
 while(capImg.isOpened()):
+    green_rect = 0
     ret, frame = capImg.read()
     if frame is None:
         break
@@ -24,12 +26,12 @@ while(capImg.isOpened()):
             box = np.int0(box)
             cv2.drawContours(frame, [box], -1, (255, 0, 0), 3)
             center = (int(rect[0][0]), int(rect[0][1]))
-            cv2.putText(capImg, "%d, %d" % (center[0], center[1]), (center[0] - 30, center[1]), cv2.FONT_HERSHEY_SIMPLEX, 1, (255, 0, 0), 1)
+            cv2.putText(frame, "%d, %d" % (center[0], center[1]), (center[0] - 30, center[1]), cv2.FONT_HERSHEY_SIMPLEX, 1, (255, 0, 0), 1)
             if center[0] < 400:
                 green_rect += 1
             else:
                 green_rect += 2
-   # cv2.putText(capImg, str(cross_way[green_rect]), (30, 30), cv2.FONT_HERSHEY_SIMPLEX, 1, (0, 255, 0), 2)
+    cv2.putText(frame, str(cross_way[green_rect]), (30, 30), cv2.FONT_HERSHEY_SIMPLEX, 1, (0, 255, 0), 2)
     cv2.imshow('hsv', hsv_msk)
     cv2.imshow('contours', frame)
     key_press = cv2.waitKey(30)
