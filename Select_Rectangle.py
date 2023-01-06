ef1442491f1b99806e5dd5d4930d80e828a675d0
Максим Валериевич Шишков <ShiMaxVal@yandex.ru>
2023-01-06 16:07:12 +0700

    SelectRectangle Cross

--- Select_Rectangle.py
+++ Select_Rectangle.py
@@ -1,5 +1,11 @@
 import numpy as np
 import cv2
+green_rect = 0
+cross_way =[{}]*4
+cross_way[0] = {"Forward"}
+cross_way[1] = {"Left"}
+cross_way[2] = {"Right"}
+cross_way[3] = {"Backward"}
 fileName = 'Cross_back.jpg'
 image = cv2.imread(fileName)
 # capImg = cv2.VideoCapture(0)
@@ -11,12 +17,17 @@ contours, hierarchy = cv2.findContours( hsv_msk, cv2.RETR_LIST, cv2.CHAIN_APPROX
 for icontour in contours:
     rect = cv2.minAreaRect(icontour)
     area = int(rect[1][0] * rect[1][1])
-    if area > 1500:
+    if area > 5000:
         box = cv2.boxPoints(rect)
         box = np.int0(box)
         cv2.drawContours(image, [box], -1, (255, 0, 0), 3)
         center = (int(rect[0][0]), int(rect[0][1]))
         cv2.putText(image, "%d, %d" % (center[0], center[1]), (center[0]-30, center[1]), cv2.FONT_HERSHEY_SIMPLEX, 1, (255, 0, 0), 2)
+        if center[0] < 400:
+            green_rect += 1
+        else: green_rect += 2
+cv2.putText(image, str(cross_way[green_rect]), (30, 30), cv2.FONT_HERSHEY_SIMPLEX, 1, (0, 255, 0), 2)
+# "%d" % (green_rect)
 cv2.imshow('hsv', hsv_msk)
 cv2.imshow('contours', image)
 cv2.waitKey()
---
+++ string_array.py
@@ -0,0 +1,11 @@
+strlist =[{}]*10
+strlist[0] = set()
+strlist[0].add("Beef")
+strlist[0].add("Fish")
+strlist[1] = {"Apple", "Banana"}
+strlist[1].add("Cherry")
+print(strlist[0])
+print(strlist[1])
+print(strlist[2])
+print("Array size:", len(strlist))
+print(strlist)
